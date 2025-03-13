86c010907f036753ff82ffa0e0605ecfe4dba42c
채근영 <chaulfe@gmail.com>
2025-03-13 19:44:51 +0900

    Update var.js

--- var.js
+++ var.js
@@ -1,7 +1,12 @@
-const a = 5;
+let a = 5;
 const b = '2';
-const myName = 'chaeyn';
+let myName = 'chaeyn';
+
 console.log(a + b);
 console.log(a * b);
 console.log(142131);
 console.log('hello ' + myName);
+
+myName = 'nicolas';
+
+console.log('your new name is ' + myName);
