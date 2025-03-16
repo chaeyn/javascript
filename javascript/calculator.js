d9e5a021b91565e7e82fdcb41da98177a4fef127
채근영 <chaulfe@gmail.com>
2025-03-16 13:55:01 +0900

    Create calculator.js

---
+++ javascript/calculator.js
@@ -0,0 +1,19 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  square: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(5, 8);
+calculator.minus(5, 8);
+calculator.divide(5, 8);
+calculator.square(5, 8);
