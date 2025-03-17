e8ddc47fb3efbfc6281fe6b3df2731cf0e00a264
채근영 <chaulfe@gmail.com>
2025-03-17 23:26:46 +0900

    Update event.js

--- javascript/event.js
+++ javascript/event.js
@@ -1,12 +1,32 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleMouseEnter() {
-  console.log("title was clicked!");
-  title.style.color = "blue";
+  console.log("h1 was clicked!");
+  h1.innerText = "Mouse is here!";
 }
 function handleTitleMouseLeave() {
-  title.style.color = "black";
+  h1.innerText = "Mouse is gone!";
 }
+function handleTitleClick() {
+  h1.style.color = "blue";
+}
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+  alert("copier!");
+}
+function handleTitleClickWindowOffline() {
+  alert("SOS no WIFI");
+}
+function handleTitleClickWindowOnline() {
+  alert("All Good");
+}
+h1.onclick = handleTitleClick;
+h1.onmouseenter = handleTitleMouseEnter;
+h1.onmouseleave = handleTitleMouseLeave;
 
-title.addEventListener("mouseenter", handleTitleMouseEnter);
-title.addEventListener("mouseleave", handleTitleMouseLeave);
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleTitleClickWindowOffline);
+window.addEventListener("online", handleTitleClickWindowOnline);
