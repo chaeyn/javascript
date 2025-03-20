7b413fcd86a29fed61a4a8fdeeacc48242631405
채근영 <chaulfe@gmail.com>
2025-03-20 20:16:26 +0900

    Update todo.js

--- learn-javascript/javascript/todo.js
+++ learn-javascript/javascript/todo.js
@@ -12,8 +12,9 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-    console.log(li.id);
     li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
@@ -50,8 +51,4 @@ if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
-}
-
-function Filter() {
-    return
 }
