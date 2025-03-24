5eb90de0cec0c2f175c151466fd0459bc95907b3
채근영 <chaulfe@gmail.com>
2025-03-25 08:48:53 +0900

    Update weather.js

--- learn-javascript/javascript/weather.js
+++ learn-javascript/javascript/weather.js
@@ -5,10 +5,8 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        const weather = document.querySelector("#weather-container span:first-child");
+        weather.innerText = `${data.main.temp}° @ ${data.name}`;
     });
 }
 function onGeoError() {
