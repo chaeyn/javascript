37081d1208f5391b115e17f55208653df0284d9c
채근영 <chaulfe@gmail.com>
2025-03-25 09:09:07 +0900

    Update todo.js

--- learn-javascript/javascript/todo.js
+++ learn-javascript/javascript/todo.js
@@ -23,10 +23,11 @@ function paintToDo(newTodo) {
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "X";
+    button.innerText = "❌";
     button.addEventListener("click", deleteToDo)
     li.appendChild(span);
     li.appendChild(button);
+    button.classList.add("button-class");
     toDoList.appendChild(li);
 }
 
