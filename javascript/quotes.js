8083e8d9ee8108daa9e0a01e98ed11a07c2c3fb8
채근영 <chaulfe@gmail.com>
2025-03-25 19:43:23 +0900

    End

--- javascript/quotes.js
+++ javascript/quotes.js
@@ -1,44 +1,47 @@
 const quotes = [
-    {
-        quote: "It always seems impossible until it’s done.",
-        author: "Nelson Mandela"
-    },
-    {
-        quote: "Do what you can, with what you have, where you are.",
-        author: "Theodore Roosevelt"
-    },
-    {
-        quote: "The only limit to our realization of tomorrow is our doubts of today.",
-        author: "Franklin D. Roosevelt"
-    },
-    {
-        quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-        author: "Winston Churchill"
-    },
-    {
-        quote: "Happiness depends upon ourselves.",
-        author: "Aristotle"
-    },
-    {
-        quote: "Believe you can and you're halfway there.",
-        author: "Theodore Roosevelt"
-    },
-    {
-        quote: "Opportunities multiply as they are seized.",
-        author: "Sun Tzu"
-    },
-    {
-        quote: "Simplicity is the ultimate sophistication.",
-        author: "Leonardo da Vinci"
-    },
-    {
-        quote: "Do what you love and you’ll never work a day in your life.",
-        author: "Confucius"
-    },
-    {
-        quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
-        author: "Aristotle"
-    }
+  {
+    quote: "It always seems impossible until it’s done.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Opportunities multiply as they are seized.",
+    author: "Sun Tzu",
+  },
+  {
+    quote: "Simplicity is the ultimate sophistication.",
+    author: "Leonardo da Vinci",
+  },
+  {
+    quote: "Do what you love and you’ll never work a day in your life.",
+    author: "Confucius",
+  },
+  {
+    quote:
+      "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Aristotle",
+  },
 ];
 
 const quote = document.querySelector("#quote span:first-child");
@@ -47,4 +50,4 @@ const author = document.querySelector("#quote span:last-child");
 const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todayQuote.quote;
-author.innerText = todayQuote.author;
+author.innerText = `- ${todayQuote.author}`;
